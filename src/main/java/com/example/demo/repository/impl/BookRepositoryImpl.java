b7066bd0f8182591c06c3962cf195407f810c1c3
Maksym Holubitskyi <golub1718@gmail.com>
2024-01-06 01:29:05 +0200

    Spring Boot web - implementer DTOs and Controllers: - added BookDto.class and CreateBookRequestDto.class - added Mapstruct and mappers - added BookController

--- src/main/java/com/example/demo/repository/impl/BookRepositoryImpl.java
+++ src/main/java/com/example/demo/repository/impl/BookRepositoryImpl.java
@@ -1,15 +1,20 @@
 package com.example.demo.repository.impl;
 
+import com.example.demo.exception.DataProcessingException;
 import com.example.demo.model.Book;
 import com.example.demo.repository.BookRepository;
 import java.util.List;
+import java.util.Optional;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class BookRepositoryImpl implements BookRepository {
+    @Autowired
     private final SessionFactory sessionFactory;
 
     public BookRepositoryImpl(SessionFactory sessionFactory) {
@@ -29,7 +34,7 @@ public class BookRepositoryImpl implements BookRepository {
             if (transaction != null) {
                 transaction.rollback();
             }
-            throw new RuntimeException("Can't add book :" + book, e);
+            throw new DataProcessingException("Can't add book :" + book, e);
         } finally {
             if (session != null) {
                 session.close();
@@ -39,11 +44,22 @@ public class BookRepositoryImpl implements BookRepository {
     }
 
     @Override
+    public Optional<Book> findBookById(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            Query<Book> bookQuery = session.createQuery("FROM Book b WHERE b.id = :id", Book.class);
+            bookQuery.setParameter("id", id);
+            return Optional.ofNullable(bookQuery.getSingleResult());
+        } catch (Exception e) {
+            throw new DataProcessingException("Can't get All Books from DB", e);
+        }
+    }
+
+    @Override
     public List<Book> findAll() {
         try (Session session = sessionFactory.openSession()) {
             return session.createQuery("FROM Book", Book.class).getResultList();
         } catch (Exception e) {
-            throw new RuntimeException("Can't get All Books from DB", e);
+            throw new DataProcessingException("Can't get All Books from DB", e);
         }
     }
 }

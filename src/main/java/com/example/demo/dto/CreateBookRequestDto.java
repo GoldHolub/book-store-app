f0d24a34faf24bd39024e4307df4be6dd0c48a8e
Maksym Holubitskyi <golub1718@gmail.com>
2024-01-11 20:31:50 +0200

    fixed data validation for book-store-app: - added anno @ISBN - added anno @NotNull in addition to @NotBlank to throw additional exception(@NotBlank can catch null, but throw only one exception: must not be blank)

--- src/main/java/com/example/demo/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/demo/dto/CreateBookRequestDto.java
@@ -2,16 +2,21 @@ package com.example.demo.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
     @NotBlank
+    @NotNull
     private String title;
     @NotBlank
+    @NotNull
     private String author;
-    @NotBlank
+    @ISBN
+    @NotNull
     private String isbn;
     @Min(0)
     private BigDecimal price;

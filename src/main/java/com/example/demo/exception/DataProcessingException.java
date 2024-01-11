e77e31e7d9cd0ec9c00a20427919ef46c720f6c6
Maksym Holubitskyi <golub1718@gmail.com>
2024-01-11 21:27:27 +0200

    Added Pagination, Sorting, and Swagger to book-store-app: - added additional parameters (Pageable) to BookController class in methods getAll(Pageable pageable) and search(BookSearchParametersDto searchParameters, Pageable pageable) - added dependencies and Anno for swagger(springdoc-openapi) - slightly modified BookSpecificationProviderManager - added additional manager for Ranges (from price - to price, from pagesAmount to pagesAmount and so on) - to be able in future sort my list of books by different ranges. Not only by String[] params(not by array of strings) - deleted DataProcessingException class (I don't have DAO classes to use it in)

--- src/main/java/com/example/demo/exception/DataProcessingException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exception;
-
-public class DataProcessingException extends RuntimeException {
-    public DataProcessingException(String message, Throwable cause) {
-        super(message, cause);
-    }
-}

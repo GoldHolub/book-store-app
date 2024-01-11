08e9eb7a2e3630f3812528728cbc19d93d2a402a
Maksym Holubitskyi <golub1718@gmail.com>
2024-01-11 14:10:30 +0200

    Added Pagination, Sorting, and Swagger to book-store-app: - added additional parameters (Pageable) to  BookController class in methods  getAll(Pageable pageable) and search(BookSearchParametersDto searchParameters, Pageable pageable) - added dependencies and Anno for swagger(springdoc-openapi) - slightly modified BookSpecificationProviderManager - added additional manager for Ranges (from price - to price, from pagesAmount to pagesAmount and so on) - to be able in future sort my list of books by different ranges. Not only by String[] params(not by array of strings)

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -4,17 +4,18 @@ import com.example.demo.dto.BookDto;
 import com.example.demo.dto.BookSearchParametersDto;
 import com.example.demo.dto.CreateBookRequestDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto bookRequestDto);
 
     BookDto getBookById(Long id);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     void deleteById(Long id);
 
     BookDto updateBookById(Long id, CreateBookRequestDto bookRequestDto);
 
-    List<BookDto> search(BookSearchParametersDto searchParameters);
+    List<BookDto> search(BookSearchParametersDto searchParameters, Pageable pageable);
 }

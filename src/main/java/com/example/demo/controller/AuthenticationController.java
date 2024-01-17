38e510439ef922e60f05d3657c392fbecbdccfca
Maksym Holubitskyi <golub1718@gmail.com>
2024-01-17 14:25:04 +0200

    bok-store-app Security changes: - implement JWT support - added login(UserLoginRequestDto request) to AuthenticationController - added AuthenticationService, JwtAuthenticationFilter, JwtUtil

--- src/main/java/com/example/demo/controller/AuthenticationController.java
+++ src/main/java/com/example/demo/controller/AuthenticationController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.user.UserLoginRequestDto;
+import com.example.demo.dto.user.UserLoginResponseDto;
 import com.example.demo.dto.user.UserRegistrationRequestDto;
 import com.example.demo.dto.user.UserResponseDto;
+import com.example.demo.security.AuthenticationService;
 import com.example.demo.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -18,11 +21,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @Operation(summary = "User registration",
             description = "register a new user")
     @PostMapping(value = "/registration")
     public UserResponseDto register(@RequestBody @Valid UserRegistrationRequestDto requestDto) {
         return userService.register(requestDto);
+    }
+
+    @PostMapping(value = "/login")
+    public UserLoginResponseDto login(@RequestBody UserLoginRequestDto requestDto) {
+        return authenticationService.authenticate(requestDto);
     }
 }
